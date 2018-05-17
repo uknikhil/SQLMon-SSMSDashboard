fa4caff132fc9c143c6e31ca60021caa16567a83
Nikhil UK <mr.nikhiluk@gmail.com>
2018-05-17 12:00:02 +0530

    Add files via upload

---
+++ Create_Credential_And_Proxy.sql
@@ -0,0 +1,15 @@
+USE [master]
+GO
+CREATE CREDENTIAL [SQLMon_Credential] WITH IDENTITY = N'DOMAIN\USER', SECRET = N'password'
+GO
+USE [msdb]
+GO
+
+EXEC msdb.dbo.sp_add_proxy @proxy_name=N'SQLMon_PSproxy',@credential_name=N'SQLMon_Credential', 
+@enabled=1
+GO
+
+EXEC msdb.dbo.sp_grant_proxy_to_subsystem @proxy_name=N'SQLMon_PSproxy', @subsystem_id=12
+GO
+
+
